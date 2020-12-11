77b513b6aed9e4003b330d49ab28d0b662a9e188
David Barcenas <daveepro@outlook.com>
2020-12-11 17:41:57 -0600

    feat: Create modal component

--- src/components/MenuLeft/index.js
+++ src/components/MenuLeft/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Icon, Menu } from 'semantic-ui-react';
 import { isUserAdmnin } from '../../utils/api';
+import { BasicModal } from '../Modal';
 
 export const MenuLeft = ({ user }) => {
   const location = useLocation();
@@ -17,35 +18,40 @@ export const MenuLeft = ({ user }) => {
   };
 
   return (
-    <Menu className="menu-left" vertical>
-      <div className="top">
-        <Menu.Item
-          as={Link}
-          to="/"
-          active={activeMenu === '/'}
-          onClick={handleMenu}
-        >
-          <Icon name="home" /> Inicio
-        </Menu.Item>
-        <Menu.Item
-          as={Link}
-          to="/artists"
-          active={activeMenu === '/artists'}
-          onClick={handleMenu}
-        >
-          <Icon name="music" /> Artistas
-        </Menu.Item>
-      </div>
-      {userAdmin && (
-        <div className="footer">
-          <Menu.Item>
-            <Icon name="plus square outline" /> Nuevo artista
+    <>
+      <Menu className="menu-left" vertical>
+        <div className="top">
+          <Menu.Item
+            as={Link}
+            to="/"
+            active={activeMenu === '/'}
+            onClick={handleMenu}
+          >
+            <Icon name="home" /> Inicio
           </Menu.Item>
-          <Menu.Item>
-            <Icon name="plus square outline" /> Nueva canción
+          <Menu.Item
+            as={Link}
+            to="/artists"
+            active={activeMenu === '/artists'}
+            onClick={handleMenu}
+          >
+            <Icon name="music" /> Artistas
           </Menu.Item>
         </div>
-      )}
-    </Menu>
+        {userAdmin && (
+          <div className="footer">
+            <Menu.Item>
+              <Icon name="plus square outline" /> Nuevo artista
+            </Menu.Item>
+            <Menu.Item>
+              <Icon name="plus square outline" /> Nueva canción
+            </Menu.Item>
+          </div>
+        )}
+      </Menu>
+      <BasicModal show={true} setShow={null} title="test title">
+        <h1>Contenido Modal</h1>
+      </BasicModal>
+    </>
   );
 };
---
+++ src/components/Modal/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Icon, Modal } from 'semantic-ui-react';
+
+export const BasicModal = ({ show, setShow, title, children }) => {
+  const handleClose = () => null; //setShow(false);
+
+  return (
+    <Modal
+      open={show}
+      onClose={handleClose}
+      className="basic-modal"
+      size="tiny"
+    >
+      <Modal.Header>
+        <h3>{title}</h3>
+        <Icon name="close" onClick={handleClose} />
+      </Modal.Header>
+      <Modal.Content>{children}</Modal.Content>
+    </Modal>
+  );
+};
