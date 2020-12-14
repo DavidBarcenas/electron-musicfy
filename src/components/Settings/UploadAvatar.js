1fcd300dc3be3379cf9e6fdf0cd5e5e56d577733
David Barcenas <daveepro@outlook.com>
2020-12-14 10:35:36 -0600

    feat(UploadAvatar): Add react-dropzone

--- src/components/Settings/UploadAvatar.js
+++ src/components/Settings/UploadAvatar.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Image } from 'semantic-ui-react';
 import NoAvatar from '../../assets/img/user.png';
+import firebase from '../../utils/firebase';
+import 'firebase/storage';
+import 'firebase/auth';
+import { useDropzone } from 'react-dropzone';
 
 export const UploadAvatar = ({ user }) => {
   const [avatarUrl, setAvatarUrl] = useState(user.photoURL);
+
+  const onDrop = useCallback((accptedFiles) => {
+    console.log(accptedFiles);
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: 'image/jpeg, image/png',
+    noKeyboard: true,
+    onDrop,
+  });
+
   return (
-    <div className="user-avatar">
-      <Image src={avatarUrl ? avatarUrl : NoAvatar} />
+    <div className="user-avatar" {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <Image src={NoAvatar} />
+      ) : (
+        <Image src={avatarUrl ? avatarUrl : NoAvatar} />
+      )}
     </div>
   );
 };
