6217dae6e92007f52880ee1d6643119dcb5dfa7a
David Barcenas <daveepro@outlook.com>
2020-12-14 11:01:40 -0600

    feat(UploadAvatar): Update user images and save changes in database

--- src/components/Settings/UploadAvatar.js
+++ src/components/Settings/UploadAvatar.js
@@ -5,12 +5,17 @@ import firebase from '../../utils/firebase';
 import 'firebase/storage';
 import 'firebase/auth';
 import { useDropzone } from 'react-dropzone';
+import { toast } from 'react-toastify';
 
 export const UploadAvatar = ({ user }) => {
   const [avatarUrl, setAvatarUrl] = useState(user.photoURL);
 
   const onDrop = useCallback((accptedFiles) => {
-    console.log(accptedFiles);
+    const file = accptedFiles[0];
+    setAvatarUrl(URL.createObjectURL(file));
+    uploadImage(file).then(() => {
+      updateUserAvatar();
+    });
   }, []);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
@@ -18,6 +23,24 @@ export const UploadAvatar = ({ user }) => {
     noKeyboard: true,
     onDrop,
   });
+
+  const uploadImage = (file) => {
+    const ref = firebase.storage().ref().child(`avatar/${user.uid}`);
+    return ref.put(file);
+  };
+
+  const updateUserAvatar = () => {
+    firebase
+      .storage()
+      .ref(`avatar/${user.uid}`)
+      .getDownloadURL()
+      .then((resp) => {
+        firebase.auth().currentUser.updateProfile({ photoURL: resp });
+      })
+      .catch(() => {
+        toast.error('Error al actualizar el avatar.');
+      });
+  };
 
   return (
     <div className="user-avatar" {...getRootProps()}>
