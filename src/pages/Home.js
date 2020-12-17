3ed548caf9e547b93f625684c7a7014ed8e49e9e
David Barcenas <daveepro@outlook.com>
2020-12-17 17:44:43 -0600

    feat(Home): Get all artists from the database

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BannerHome } from '../components/BannerHome';
+import firebase from '../utils/firebase';
+import 'firebase/firestore';
 
 export const Home = () => {
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('artists')
+      .get()
+      .then((data) => {
+        const arrayArtists = [];
+        if (data.docs.length > 0) {
+          data.docs.map((artist) => {
+            const data = artist.data();
+            data.id = artist.id;
+            arrayArtists.push(data);
+          });
+          setArtists(arrayArtists);
+        }
+      });
+  }, []);
+
   return (
     <div className="home">
       <BannerHome />
