20257d09dc883ef03776a26f9fc3c26c7c4c236c
David Barcenas <daveepro@outlook.com>
2020-12-17 20:53:55 -0600

    feat(BasicSlider): Show artists in slider

--- src/components/Sliders/BasicSlider.js
+++ src/components/Sliders/BasicSlider.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
+import firebase from '../../utils/firebase';
+import 'firebase/storage';
 
 export const BasicSlider = ({ title, data }) => {
   const settings = {
@@ -14,7 +16,32 @@ export const BasicSlider = ({ title, data }) => {
   return (
     <div className="basic-slider">
       <h2>{title}</h2>
-      <Slider {...settings}></Slider>
+      <Slider {...settings}>
+        {data.map((item) => (
+          <SliderItem key={item.id} item={item} />
+        ))}
+      </Slider>
     </div>
   );
 };
+
+function SliderItem({ item }) {
+  const [imageURL, setImageURL] = useState(null);
+  useEffect(() => {
+    firebase
+      .storage()
+      .ref(`artists/${item.banner}`)
+      .getDownloadURL()
+      .then((url) => setImageURL(url));
+  }, [item]);
+
+  return (
+    <div className="basic-slider-item">
+      <div
+        className="basic-slider-item-img"
+        style={{ backgroundImage: `url(${imageURL})` }}
+      ></div>
+      <h3>{item.name}</h3>
+    </div>
+  );
+}
