62f4377c0646d52dd3d2ff7f29c530fdc9fc6d96
David Barcenas <daveepro@outlook.com>
2020-12-27 22:09:58 -0600

    feat(Artists): Get artist images

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Grid } from 'semantic-ui-react';
 import firebase from '../utils/firebase';
 import 'firebase/firestore';
 
@@ -20,7 +21,24 @@ export const Artists = () => {
 
   return (
     <div>
-      <h2>Artistas</h2>
+      <h1>Artistas</h1>
+      <Grid>
+        {artists.map((artist) => (
+          <Artist key={artist.id} artist={artist} />
+        ))}
+      </Grid>
     </div>
   );
 };
+
+function Artist({ artist }) {
+  const [banner, setBanner] = useState(null);
+  useEffect(() => {
+    firebase
+      .storage()
+      .ref(`artists/${artist.banner}`)
+      .getDownloadURL()
+      .then((url) => setBanner(url));
+  }, [artist]);
+  return <div>hola</div>;
+}
