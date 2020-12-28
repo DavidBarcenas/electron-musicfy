a71eed09cdb4ca59b918b962b99b69c5514bd5d5
David Barcenas <daveepro@outlook.com>
2020-12-27 22:22:06 -0600

    style(Artists): Show a grid of artists

--- src/pages/Artists.js
+++ src/pages/Artists.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Grid } from 'semantic-ui-react';
 import firebase from '../utils/firebase';
 import 'firebase/firestore';
+import { Link } from 'react-router-dom';
 
 export const Artists = () => {
   const [artists, setArtists] = useState([]);
@@ -20,11 +21,13 @@ export const Artists = () => {
   }, []);
 
   return (
-    <div>
+    <div className="artists">
       <h1>Artistas</h1>
       <Grid>
         {artists.map((artist) => (
-          <Artist key={artist.id} artist={artist} />
+          <Grid.Column key={artist.id} mobile={8} table={4} computer={3}>
+            <Artist artist={artist} />
+          </Grid.Column>
         ))}
       </Grid>
     </div>
@@ -40,5 +43,15 @@ function Artist({ artist }) {
       .getDownloadURL()
       .then((url) => setBanner(url));
   }, [artist]);
-  return <div>hola</div>;
+  return (
+    <Link to={`/artist/${artist.id}`}>
+      <div className="artists-item">
+        <div
+          className="artists-avatar"
+          style={{ backgroundImage: `url(${banner})` }}
+        ></div>
+        <h3>{artist.name}</h3>
+      </div>
+    </Link>
+  );
 }
