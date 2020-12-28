5915523b35a3b8aea01877ff01e860ba665e8ccb
David Barcenas <daveepro@outlook.com>
2020-12-27 21:12:44 -0600

    feat(Artist): Get artist information from database

--- src/pages/Artist.js
+++ src/pages/Artist.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import firebase from '../utils/firebase';
+import 'firebase/firestore';
 
 export const Artist = () => {
-  const params = useParams();
-  console.log(params);
+  const [artist, setArtist] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('artists')
+      .doc(id)
+      .get()
+      .then((artist) => setArtist(artist.data()));
+  }, [id]);
+
   return (
     <div>
       <h1>Artist</h1>
