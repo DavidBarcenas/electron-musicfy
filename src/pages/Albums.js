d70b0811ff298e21af42a37c64c2652a24a3806e
David Barcenas <daveepro@outlook.com>
2021-01-02 16:20:30 -0600

    feat(Albums): Get albums from database

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from '../utils/firebase';
+import 'firebase/firestore';
 
 export const Albums = () => {
+  const [albums, setAlbums] = useState([]);
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('albums')
+      .get()
+      .then((data) => {
+        const arrayAlbums = [];
+        if (data.docs.length > 0) {
+          data.docs.map((album) => {
+            const data = album.data();
+            data.id = album.id;
+            return arrayAlbums.push(data);
+          });
+          setAlbums(arrayAlbums);
+        }
+      });
+  }, []);
   return (
-    <div>
-      <h1>Albums</h1>
+    <div className="albums">
+      <h1>Álbumes</h1>
     </div>
   );
 };
