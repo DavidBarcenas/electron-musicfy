ccadb2523b6fa0a7215aa4b53c250f848a866bdb
David Barcenas <daveepro@outlook.com>
2021-01-02 16:33:45 -0600

    style(Albums): Create album layout

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { Grid } from 'semantic-ui-react';
 import firebase from '../utils/firebase';
 import 'firebase/firestore';
+import 'firebase/storage';
+import { Link } from 'react-router-dom';
 
 export const Albums = () => {
   const [albums, setAlbums] = useState([]);
@@ -24,6 +27,40 @@ export const Albums = () => {
   return (
     <div className="albums">
       <h1>Álbumes</h1>
+      <Grid>
+        {albums.map((album) => (
+          <Grid.Column key={album.id} mobile={8} tablet={4} computer={3}>
+            <Album album={album} />
+          </Grid.Column>
+        ))}
+      </Grid>
     </div>
   );
 };
+
+function Album({ album }) {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    firebase
+      .storage()
+      .ref(`album/${album.banner}`)
+      .getDownloadURL()
+      .then((url) => {
+        setImageUrl(url);
+      })
+      .catch((err) => console.log('No se pudo obtener la imagen.'));
+  }, [album]);
+
+  return (
+    <Link to={`/album/${album.id}`}>
+      <div className="album-item">
+        <div
+          className="avatar"
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        ></div>
+        <h3>{album.name}</h3>
+      </div>
+    </Link>
+  );
+}
