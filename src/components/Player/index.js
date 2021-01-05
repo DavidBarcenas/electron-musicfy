d5163c9bd65b2e63e0218dc1330d007518b67753
David Barcenas <daveepro@outlook.com>
2021-01-04 21:04:06 -0600

    feat(player): Create the structure

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
 
 export const Player = () => {
   return (
-    <div>
-      <h1>Player</h1>
+    <div className="player">
+      <Grid>
+        <Grid.Column width={4} className="left">
+          Left
+        </Grid.Column>
+        <Grid.Column width={8} className="center">
+          Center
+        </Grid.Column>
+        <Grid.Column width={4} className="right">
+          Right
+        </Grid.Column>
+      </Grid>
     </div>
   );
 };
