42b40ea62e85c74c47588355db8f546a4e384d39
David Barcenas <daveepro@outlook.com>
2021-01-05 17:31:23 -0600

    feat(AddSongForm): Get all albums

--- src/components/Songs/AddSongForm.js
+++ src/components/Songs/AddSongForm.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Dropdown, Form, Input } from 'semantic-ui-react';
+import firebase from '../../utils/firebase';
+import 'firebase/firestore';
 
 const onSubmit = () => {
   console.log('enviando formulario...');
 };
 
 export const AddSongForm = (setShowModal) => {
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('albums')
+      .get()
+      .then((resp) => {
+        const arrayAlbums = [];
+        resp?.docs.map((album) => {
+          return arrayAlbums.push({
+            key: album.id,
+            value: album.id,
+            text: album.data().name,
+          });
+        });
+        setAlbums(arrayAlbums);
+      });
+  }, []);
+
   return (
     <Form className="add-song-form" onSubmit={onSubmit}>
       <Form.Field>
@@ -17,7 +39,7 @@ export const AddSongForm = (setShowModal) => {
           search
           selection
           lazyLoad
-          options={[]}
+          options={albums}
         />
       </Form.Field>
       <Form.Field>
