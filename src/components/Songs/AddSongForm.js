9923f98566de236eb8d08111cacc58937177dff1
David Barcenas <daveepro@outlook.com>
2021-01-05 17:22:36 -0600

    feat(AddSongForm): Create form

--- src/components/Songs/AddSongForm.js
+++ src/components/Songs/AddSongForm.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, Dropdown, Form, Input } from 'semantic-ui-react';
+
+const onSubmit = () => {
+  console.log('enviando formulario...');
+};
 
 export const AddSongForm = (setShowModal) => {
   return (
-    <div>
-      <h1>AddSongForm</h1>
-    </div>
+    <Form className="add-song-form" onSubmit={onSubmit}>
+      <Form.Field>
+        <Input placeholder="Nombre de la canción" />
+      </Form.Field>
+      <Form.Field>
+        <Dropdown
+          placeholder="Asigna la canción a un álbum"
+          search
+          selection
+          lazyLoad
+          options={[]}
+        />
+      </Form.Field>
+      <Form.Field>
+        <div>UploadSong</div>
+      </Form.Field>
+      <Button type="submit">Subir canción</Button>
+    </Form>
   );
 };
