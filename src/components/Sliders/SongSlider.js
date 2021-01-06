67734158ff3073a6d3360a42423f8a9380207db9
David Barcenas <daveepro@outlook.com>
2021-01-05 19:24:10 -0600

    feat(SongSlider): Create song slider

--- src/components/Sliders/SongSlider.js
+++ src/components/Sliders/SongSlider.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import Slider from 'react-slick';
 
 export const SongSlider = ({ title, data }) => {
+  const settings = {
+    dots: false,
+    infinity: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 4,
+    centerMode: true,
+    className: 'song-slider-list',
+  };
+
+  if (data.length < 5) {
+    return null;
+  }
+
   return (
     <div className="songs-slider">
       <h2>{title}</h2>
+      <Slider {...settings}>
+        {data.map((item) => (
+          <SliderItem key={item.id} item={item} />
+        ))}
+      </Slider>
     </div>
   );
 };
+
+function SliderItem({ item }) {
+  return <div>{item.name}</div>;
+}
